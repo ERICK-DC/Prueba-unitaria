6fa709a2e865b715c69e8cc2cad8529004d99641
ERICK-DC <67754832+ERICK-DC@users.noreply.github.com>
2020-12-10 21:17:57 -0500

    prueba unitaria
    prueba unitaria

---
+++ Matematica.Cli/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Matematica.Cli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese el primer número");
+            var primerNumero = Console.ReadLine();
+            Console.WriteLine("Ingrese el segundo número");
+            var segundoNumero = Console.ReadLine();
+            Console.WriteLine("La suma de los dos números es: ");
+            var calculadora = new Matematica.Negocio.Calculadora();
+            var resultado = calculadora.Sumar(int.Parse(primerNumero), int.Parse(segundoNumero));
+            Console.WriteLine("La suma de los dos números es: "+ resultado);
+        }
+    }
+}
---
+++ MatematicaPrueba.Test/CalculadoraTest.cs
@@ -0,0 +1,37 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Matematica.Negocio;
+
+
+namespace Matematica.Test
+{
+    [TestClass]
+    public class CalculadoraTest
+    {
+        [TestMethod]
+        public void SumaDeDosNumeros()
+        {
+            var primerNumero = 18;
+            var segundoNumero = 3.7;
+            var esperadoComplejo = 21.7;
+
+            var miCalculadora = new Calculadora();
+            var resultado= miCalculadora.Sumar(primerNumero, segundoNumero);
+
+            Assert.AreEqual(esperadoComplejo, resultado);
+        }
+
+        [TestMethod]
+        public void SumarComplejos()
+        {
+            var primerComplejo = new Complejo(3,2);
+            var segundoComplejo = new Complejo(1.5,6);
+            var esperadoComplejo = new Complejo(4.5,8);
+
+            var miCalculadora = new Calculadora();
+            var resultado = miCalculadora.Sumar(primerComplejo, segundoComplejo);
+
+            Assert.AreEqual(esperadoComplejo, resultado);
+
+        }
+    }
+}
